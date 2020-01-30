c75fcb73b8a1839f2e6f4ad5a4f5042c8e1c1200
mineZ1324 <60462070+mineZ1324@users.noreply.github.com>
2020-01-30 15:54:33 +0500

    Add files via upload

---
+++ Морской бой/Assets/Scripts/Dispatcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dispatcher : MonoBehaviour
+{
+    static Dictionary<string, int> shipsLeftToAllocate = new Dictionary<string, int>();
+    public GameObject shipPrefab;
+    public static Ship currentShip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var floorsNumStr = gameObject.name.Replace("Ship-", null);
+        var shipsToAllocate = 5 - int.Parse(floorsNumStr);
+        if (!shipsLeftToAllocate.ContainsKey(gameObject.name))
+        {
+            shipsLeftToAllocate.Add(gameObject.name, shipsToAllocate);
+        }
+    }
+
+    public void OnShipClick()
+    {
+        if (currentShip == null) // Обычный шаблон
+        {
+            var shipObjToPlay = Instantiate(shipPrefab, transform.parent.transform);
+            currentShip = shipObjToPlay.GetComponentInChildren<Ship>();
+        }
+    }
+}
---
+++ Морской бой/Assets/Scripts/GameField.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameField : MonoBehaviour
+{
+    public enum CellState
+    {
+        Empty, Misdelivered, Occupied, Misplaced, Hit
+    }
+
+
+    public GameObject cellPrefab;
+    public float bottomLeftX, bottomLeftY;
+    static Bounds[,] BoundsOfCells;
+    static int[,] fieldBody = new int[10, 8];
+    static float cellSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sprRenderer = cellPrefab.GetComponent<SpriteRenderer>();
+        cellSize = sprRenderer.bounds.size.x;
+        BoundsOfCells = new Bounds[Width(), Heigth()];
+        GenerateField();
+    }
+
+    void GenerateField()
+    {
+        for (int i = 0; i < Width(); i++)
+        {
+            for (int j = 0; j < fieldBody.GetLength(1); j++)
+            {
+                var cellPos = new Vector2(bottomLeftX + i * cellSize, bottomLeftY + j * cellSize);
+                Instantiate(cellPrefab, cellPos, Quaternion.identity);
+                var CellBounds = new Bounds(cellPos, new Vector2(cellSize, cellSize));
+                BoundsOfCells[i, j] = CellBounds;
+            }
+        }
+    }
+
+    static int Width()
+    {
+        return fieldBody.GetLength(0);
+    }
+
+    static int Heigth()
+    {
+        return fieldBody.GetLength(1);
+    }
+
+    public static void CheckShipPosition(Vector3 mousePos, Ship ship)
+    {
+        var BottomLeftCells = BoundsOfCells[0, 0];
+        var UpperRightCells = BoundsOfCells[Width() - 1, Heigth() - 1];
+        var BottomLeftCorner = BottomLeftCells.min;
+        var UpperRightCorner = UpperRightCells.max;
+        bool IsOverField = mousePos.x>BottomLeftCorner.x && mousePos.y>BottomLeftCorner.y && mousePos.x<UpperRightCorner.x && mousePos.y<UpperRightCorner.y;
+
+        if (!IsOverField)
+        {
+            ship.IsPositionCorrect = false;
+            return;
+        }
+        var dx = mousePos.x-BottomLeftCorner.x;
+        var dy = mousePos.y - BottomLeftCorner.y;
+        
+        int x = (int)(dx/cellSize);
+        int y = (int)(dy / cellSize);
+        //Debug.Log(x+" , "+y);
+        ship.IsPositionCorrect = true;
+        ship.IsWithIn = true;
+        ship.CellCenterPos = BoundsOfCells[x,y].center;
+        //Debug.Log(y);
+
+    }
+
+}
